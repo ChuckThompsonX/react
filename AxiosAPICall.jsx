90755a0f8dc19eecb9a5f7feda9dc753d006aa8c
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-15 08:29:58 -0800

    Create AxiosAPICall.jsx

---
+++ AxiosAPICall.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// most up-to-date lib for react
+// npm install axios
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://chuckthompsonx.com')
+      .then(resp => setPosts(resp.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {posts.map(post => (<li key={post.id}>{post.content}</li>))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
