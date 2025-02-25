e4c144237c538c7883d5cacd1617c87425e3e4ae
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-02-24 21:00:59 -0800

    Create ArrayChanges.jsx

---
+++ ArrayChanges.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function ArrayChanges() {
+  
+  // initialize elements state with array of objects
+  const [elements, setElements] = useState([
+    { id: 1, name: "Element 1" },
+    { id: 2, name: "Element 2" },
+  ]);
+
+  // add element to array with max id value
+  const addElement= () => {
+    const maxId = elements.reduce((max, obj) => Math.max(max, obj.id), 0);
+    setElements([...elements, { id: maxId + 1, name: `Element ${maxId + 1}` }]);
+  };
+
+  // remove id element from array
+  const removeElement = (id) => {
+    setElements(elements.filter((element) => element.id !== id));
+  };
+
+  return (
+    <div>
+      <button onClick={addElement}>Add Element</button>
+      <ul>
+        {elements.map((element) => (
+          <li key={element.id}>
+            {element.name}
+            <button onClick={() => removeElement(element.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ArrayChanges;
