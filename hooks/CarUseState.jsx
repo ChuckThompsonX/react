634502343b6daf768447bc14a6fd85e78a651513
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-05 08:31:05 -0800

    Create CarUseState.jsx

---
+++ hooks/CarUseState.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const CarUseState = () => {
+  const [car, setCar] = useState({
+    make: "Chevy",
+    model: "Camaro",
+    color: "red",
+  });
+
+  const changeColor = () => {
+    // typically when state is updated, the entire state gets overwritten
+    // for instance, if we only changed the color it will remove the make and model from the state
+    // pass a function into the setCar call which receives the entire current car state 
+    // by spreading the carState object and overwriting only color
+    setCar((carState) => {
+      return { ...carState, color: "blue" };
+    });
+  };
+
+  return (
+    <>
+      <p>My {car.make} {car.model} is {car.color}.</p>
+      <button type="button" onClick={changeColor}>Change Car Color</button>
+    </>
+  );
+}
+
+export default CarUseState;
