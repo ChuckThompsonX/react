bb5e4140312fc3ea59520cf069a6745486c3de0f
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-11 19:16:55 -0700

    Create AccessProfile.jsx

---
+++ AccessProfile.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const AccessProfile = () =>{
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const AnotherComponent = "User Profile Info";
+
+  return (
+    <div>
+      {isAuthenticated ? (
+        <h2>{AnotherComponent}</h2>
+      ) : (
+        <button onClick={() => setIsAuthenticated(true)}>Access Profile</button>
+      )}
+    </div>
+  );
+}
+
+export default AccessProfile;
