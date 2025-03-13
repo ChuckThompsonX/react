4d497b9f5e331e31872108534d0d105a8936437d
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-03-12 20:22:55 -0700

    Create Header.jsx

---
+++ econnectwith/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center justify-center group rounded text-sm font-semibold cursor-pointer p-0.5">
+        <h1 className="font-extrabold text-transparent text-3xl sm:text-4xl xl:text-5xl bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-transparent h-full">
+          EConnectWith
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
